44e09a3228884ea1aee084b4321869728373e2dd
Lina-Anna <62033643+Lina-Anna@users.noreply.github.com>
2020-11-22 22:20:56 +0500

    проблемы

--- main.c
+++ main.c
@@ -7,41 +7,40 @@
 #include <time.h>
 #include <ctype.h>
 
-#define STRINGS_COUNT   1000000
+#define STRINGS_COUNT   10
 #define MAX_STRING_SIZE 100
 
+char name = '0';
 unsigned char strings[STRINGS_COUNT][MAX_STRING_SIZE];
 
 
 void GenerateRandomString(char* str, unsigned int max_size) {
 
-   
-    unsigned int size = rand() % (max_size+1);
-    if (size == 0) {
-        size++;
-    }
- 
-    //printf("%d\t", size);
-    // генерация одной случайной строки
-    for (int i = 0; i < size; i++) {
-        str[i] = rand()%(109 - 48 + 1)+48; 
-        if ((str[i] > 57)&&(str[i]<=83)) {
-            str[i] += 7;
-        }
-        else if (str[i] >= 84) {
-            str[i] +=13;
-        }
-        //str[i] = 89;
-    }
-    /*for (int i = 0; i < size; i++) {
-        str[i] = rand() % (57 - 48 + 1) + 48;
-    }*/
-    if (size < max_size) {
-        str[size] = '\0';
-    }
     
-   
 
+    
+        unsigned int size = rand() % (max_size + 1);
+        if (size == 0) {
+            size++;
+        }
+
+        for (int i = 0; i < size; i++) {
+            str[i] = rand() % (109 - 48 + 1) + 48;
+            if ((str[i] > 57) && (str[i] <= 83)) {
+                str[i] += 7;
+            }
+            else if (str[i] >= 84) {
+                str[i] += 13;
+            }
+            //str[i] = 89;
+        }
+
+        if (size < max_size) {
+            str[size] = '\0';
+        }
+
+        
+    
 }
 
 
@@ -70,16 +69,129 @@ void PrintStrings(char strings[][MAX_STRING_SIZE]) {
 
 }
 
-int test_str_1(char  str_1[MAX_STRING_SIZE] , char  str_2[MAX_STRING_SIZE]) {
-    int kol_pr=0 ;
-    /*if ((str_1[0] > str_2[0])) {
-        k = 1;
-    }
-    return(k);*/
-    for (int i = 0; i < MAX_STRING_SIZE-kol_pr; i++) {
-    //for (int i = 0; i < 2; i++) {
+//int test_str_1(char  str_1[MAX_STRING_SIZE] , char  str_2[MAX_STRING_SIZE]) {
+//    int kol_pr=0 ;
+//    /*if ((str_1[0] > str_2[0])) {
+//        k = 1;
+//    }
+//    return(k);*/
+//    for (int i = 0; i < MAX_STRING_SIZE-kol_pr; i++) {
+//    //for (int i = 0; i < 2; i++) {
+//        if (str_1[i] == '\0') {
+//            return(0);
+//        }
+//        else if (str_2[i] == '\0') {
+//            return(1);
+//        }
+//        else if (str_1[i] > str_2[i]) {
+//            return(1);
+//        }
+//        else if (str_1[i] < str_2[i]) {
+//            return(0);
+//        }
+//        kol_pr++;
+//
+//        
+//    }
+//    return(0);
+//}
+//int test_str_2(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
+//    int kol_pr = 0;
+//    for (int i = 0; i < MAX_STRING_SIZE - kol_pr; i++) {
+//        //for (int i = 0; i < 2; i++) {
+//        if (str_1[i] == '\0') {
+//            return(0);
+//        }
+//        else if (str_2[i] == '\0') {
+//            return(1);
+//        }
+//        else if ((tolower(str_1[i])) > (tolower( str_2[i]))) {
+//            return(1);
+//        }
+//        else if ((tolower(str_1[i])) < (tolower(str_2[i]))) {
+//            return(0);
+//        }
+//        kol_pr++;
+//
+//
+//    }
+//    return(0);
+//}
+//int test_str_3(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
+//    int kol_pr = 0;
+//    int max_1 = strlen(str_1), max_2 = strlen(str_2);
+//    if (max_1 > MAX_STRING_SIZE) {
+//        max_1 = MAX_STRING_SIZE;
+//    }
+//    if (max_2 > MAX_STRING_SIZE) {
+//        max_2 = MAX_STRING_SIZE;
+//    }
+// 
+//    if (max_1 > max_2) {
+//        return(1);
+//    }
+//    else if (max_1 == max_2) {
+//        return(test_str_1(str_1,str_2));
+//    }
+//    else {
+//       return(0);
+//    }
+//    
+//   
+//   
+//}
+//int test_str_4(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
+//    int kol_pr = 0;
+//    for (int i = 0; i < MAX_STRING_SIZE - kol_pr; i++) {
+//        //for (int i = 0; i < 2; i++) {
+//        if (str_2[i] == '\0') {
+//            return(0);
+//        }
+//        else if (str_1[i] == '\0') {
+//            return(1);
+//        }
+//        else if (str_1[i] < str_2[i]) {
+//            return(1);
+//        }
+//        else if (str_1[i] > str_2[i]) {
+//            return(0);
+//        }
+//        kol_pr++;
+//
+//
+//    }
+//    return(0);
+//}
+//int test_str_5(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
+//    int kol_pr = 0;
+//    int max_1 = strlen(str_1), max_2 = strlen(str_2);
+//    if (max_1 > MAX_STRING_SIZE) {
+//        max_1 = MAX_STRING_SIZE;
+//    }
+//    if (max_2 > MAX_STRING_SIZE) {
+//        max_2 = MAX_STRING_SIZE;
+//    }
+//
+//    if (max_1 < max_2) {
+//        return(1);
+//    }
+//    else if (max_1 == max_2) {
+//        return(test_str_4(str_1, str_2));
+//    }
+//    else {
+//        return(0);
+//    }
+//
+//
+//
+//}
+
+int test_str_1(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
+    int kol_pr = 0;
+    for (int i = 0; i < MAX_STRING_SIZE - kol_pr; i++) {
+        //for (int i = 0; i < 2; i++) {
         if (str_1[i] == '\0') {
-            return(0);
+            return(-1);
         }
         else if (str_2[i] == '\0') {
             return(1);
@@ -88,12 +200,11 @@ int test_str_1(char  str_1[MAX_STRING_SIZE] , char  str_2[MAX_STRING_SIZE]) {
             return(1);
         }
         else if (str_1[i] < str_2[i]) {
-            return(0);
+            return(-1);
         }
         kol_pr++;
-
-        
     }
+    
     return(0);
 }
 int test_str_2(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
@@ -101,16 +212,16 @@ int test_str_2(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
     for (int i = 0; i < MAX_STRING_SIZE - kol_pr; i++) {
         //for (int i = 0; i < 2; i++) {
         if (str_1[i] == '\0') {
-            return(0);
+            return(-1);
         }
         else if (str_2[i] == '\0') {
             return(1);
         }
-        else if ((tolower(str_1[i])) > (tolower( str_2[i]))) {
+        else if ((tolower(str_1[i])) > (tolower(str_2[i]))) {
             return(1);
         }
         else if ((tolower(str_1[i])) < (tolower(str_2[i]))) {
-            return(0);
+            return(-1);
         }
         kol_pr++;
 
@@ -127,26 +238,26 @@ int test_str_3(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
     if (max_2 > MAX_STRING_SIZE) {
         max_2 = MAX_STRING_SIZE;
     }
- 
+
     if (max_1 > max_2) {
         return(1);
     }
     else if (max_1 == max_2) {
-        return(test_str_1(str_1,str_2));
+        return(test_str_1(str_1, str_2));
     }
     else {
-       return(0);
+        return(-1);
     }
-    
-   
-   
+
+
+
 }
 int test_str_4(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
     int kol_pr = 0;
     for (int i = 0; i < MAX_STRING_SIZE - kol_pr; i++) {
         //for (int i = 0; i < 2; i++) {
         if (str_2[i] == '\0') {
-            return(0);
+            return(-1);
         }
         else if (str_1[i] == '\0') {
             return(1);
@@ -155,7 +266,7 @@ int test_str_4(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
             return(1);
         }
         else if (str_1[i] > str_2[i]) {
-            return(0);
+            return(-1);
         }
         kol_pr++;
 
@@ -180,38 +291,79 @@ int test_str_5(char  str_1[MAX_STRING_SIZE], char  str_2[MAX_STRING_SIZE]) {
         return(test_str_4(str_1, str_2));
     }
     else {
-        return(0);
+        return(-1);
     }
 
 
 
 }
 
+//void SortStrings(char strings[][MAX_STRING_SIZE], int (* compareStringsFunction)(char * ,char *)) {
+//
+//   
+//    int k = 0;
+//    char  temp=0;
+//    int f = 1;
+//    do {
+//        f = 0;
+//        for (int i = 1; i < STRINGS_COUNT; i++) {
+//            //k = test_str(strings[i - 1], strings[i]);
+//            k= compareStringsFunction(strings[i - 1], strings[i]);
+//            if (k == 1) {
+//                for (int j = 0; j < MAX_STRING_SIZE; j++) {
+//                    temp = strings[i - 1][j]; // 
+//                    strings[i - 1][j] = strings[i][j];
+//                    strings[i][j] = temp;
+//                }
+//                f = 1;
+//
+//            }
+//        }
+//       
+//    } while (f == 1);
+//
+//}
 
-void SortStrings(char strings[][MAX_STRING_SIZE], int (* compareStringsFunction)(char * ,char *)) {
-
-   
-    int k = 0;
-    char  temp=0;
-    int f = 1;
-    do {
-        f = 0;
-        for (int i = 1; i < STRINGS_COUNT; i++) {
-            //k = test_str(strings[i - 1], strings[i]);
-            k= compareStringsFunction(strings[i - 1], strings[i]);
-            if (k == 1) {
-                for (int j = 0; j < MAX_STRING_SIZE; j++) {
-                    temp = strings[i - 1][j]; // 
-                    strings[i - 1][j] = strings[i][j];
-                    strings[i][j] = temp;
-                }
-                f = 1;
-
-            }
-        }
+void qsort_t( int b, int e, int(*compareStringsFunction)(char*, char*))
+{
+    int l = b, r = e;
+    int p = (l + r) / 2;
+    //char* temp;
+    char t =0;
+    char piv[MAX_STRING_SIZE];// Опорным элементом для примера возьмём средний
+    
+    //strcpy_s(piv, MAX_STRING_SIZE, strings[p]);
+    for (int k = 0; k < MAX_STRING_SIZE; k++) {
+        piv[k] = strings[p][k];
+    }
+    while (l <= r)
+    {
        
-    } while (f == 1);
+        while ((compareStringsFunction(strings[l], piv) < 0)&&(l<r)&&(l<STRINGS_COUNT-1)) {
 
+            l++;
+        }
+
+        while ((compareStringsFunction(strings[r], piv) > 0)&&(r>l)&&(r>0)){
+            r--;
+        }
+        if (l <= r) {
+            
+            for (int ii=0; ii < MAX_STRING_SIZE; ii++) {
+                t = strings[l][ii];
+                strings[l][ii] = strings[r][ii];
+                strings[r][ii] = t;
+            }
+            l++;
+            r--;
+        }
+    }
+    if (b < r) {
+        qsort_t( b, r, compareStringsFunction);
+    }
+    if (e > l) {
+        qsort_t(l, e, compareStringsFunction);
+    }
 }
 
 
@@ -244,9 +396,11 @@ int main() {
         exit(1);
     }
     GenerateRandomStrings(strings);
-   // PrintStrings(strings);
-    SortStrings(strings, compare);
-    //PrintStrings(strings);
+    PrintStrings(strings);
+    //SortStrings(strings, compare);
+    qsort_t( 0,STRINGS_COUNT-1 , compare);
+    printf("\n");
+    PrintStrings(strings);
 
     return 0;
 }
